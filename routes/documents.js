b62ba7cb652d4aae9136d7afe717c993321169d1
korobokvk <korobokvk@gmail.com>
2018-10-10 10:55:16 +0300

    refactoring

--- routes/documents.js
+++ routes/documents.js
@@ -3,50 +3,49 @@ const router = express.Router();
 
 const elastic = require('../elasticsearch');
 
-        router.get('/docs', (req, res, next) => {
-            elastic.search().then(data => {
-                res.json(data.hits.hits);
-            }).catch(err => {
-                console.error(err);
-            })
-        });
+router.get('/docs', (req, res, next) => {
+  elastic.search().then(data => {
+    res.status(200).json(data.hits.hits);
+  }).catch(err => {
+    console.error(err);
+    res.status(500).json(err)
+  })
+});
 
-        router.post('/docs', (req, res, next) => {
-            elastic.addDocs(req.body).then(data => {
-                res.sendStatus(200);
-                router.get('/docs/:id', (req, res, next) => {
-                    elastic.searchById(req.params.id).then(data => {
-                        res.json(data);
-                    })
-                });
-            })
-        });
+router.get('/docs/:id', (req, res, next) => {
+  elastic.searchById(req.params.id).then(data => {
+    res.status(200).json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
-        router.post('/docs', (req, res, next) => {
-            elastic.addDocs(req.body).then(data => {
-                res.json(data)
-            }).catch(err => {
-                console.log(err);
-                res.sendStatus(500);
-            });
-        });
+router.post('/docs', (req, res, next) => {
+  elastic.addDocs(req.body).then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
-        router.delete('/docs/:id', (req, res, next) => {
-            elastic.deleteIndex(req.params.id).then(data => {
-                res.sendStatus(200);
-            }).catch(err => {
-                console.log(err);
-                res.sendStatus(500);
-            });
-        });
+router.delete('/docs/:id', (req, res, next) => {
+  elastic.deleteIndex(req.params.id).then(data => {
+    res.sendStatus(200);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
 
-        router.put('/docs/:id', (req, res, next) => {
-            elastic.addDocs(req.body, req.params.id).then(data => {
-                res.json(data)
-            }).catch(err => {
-                console.log(err);
-                res.sendStatus(500);
-            });
-        });
+router.put('/docs/:id', (req, res, next) => {
+  elastic.addDocs(req.body, req.params.id).then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
